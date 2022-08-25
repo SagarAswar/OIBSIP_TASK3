eb31a8768ebb94b32ac1b625e2fa92882f3faea8
Sagar Aswar <SagarAswar001@gmail.com>
2022-08-26 00:46:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,5 @@
 const convertBtn = document.querySelector("#convertBtn");
-// const resetBtn = document.querySelector("#resetBtn");
-// var displayResult = document.querySelector("#displayResult");
-// var input = document.querySelector("#input");
 const option = document.querySelector("option");
-
-// const resetFunction = () => {
-//     input.value = "";
-//     input.innerHTML = ""
-// }
-
-// const convertToFahr = () => {
-//     let result = ((input.value * 1.8) + 32);
-//     displayResult.innerHTML = `${result}`
-// }
-
-// const convertToCelc = () => {
-//     let result = (((input.value - 32) * 5) / 9)
-// displayResult.textContent = `${result}`
-// }
-
-
-
-
-// convertFahrenheit = () => {
-//     var celsius = (document.querySelector("#celsius").value);
-
-//     var fahrenheitResult = (1.8 * celsius) + 32;
-//     var a = fahrenheitResult.toFixed(2);
-
-//     document.querySelector("#celsius").value = celsius;
-//     document.querySelector("#fahrenheit").value = a;
-// }
-
-// convertCelsius = () => {
-//     var fahrenheit = parseFloat(document.querySelector("#fahrenheit").value);
-
-//     var celsiusResult = (fahrenheit - 32) / 1.8;
-//     var b = celsiusResult.toFixed(2);
-
-//     document.querySelector("#celsius").value = b;
-//     document.querySelector("#fahrenheit").value = fahrenheit;
-// }
-
-// reset = () => {
-//     document.querySelector("#celsius").value = " ";
-//     document.querySelector("#fahrenheit").value = " ";
-// }
 
 const convertToFahr = () => {
     var input = document.querySelector("#input");
